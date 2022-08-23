5edf7069d58f908277caeb868019e2660cf02791
ali.tayebi <ali.tayebi@educationperfect.com>
2022-08-23 12:53:58 +1000

    Adds pack and publish actions

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# avro-nuget
-Houses Github actions to generate C# code from Avro schemas and pack them as a NuGet package
+# Avro NuGet GitHub Action
+This repository houses GitHub actions to Nuget Packages out of Avro schema files and publish them.
---
+++ pack/entrypoint.sh
@@ -0,0 +1,82 @@
+#!/bin/sh -l
+
+set -e
+
+SRC=./src
+
+for i in "$@"; do
+  case $i in
+    -n=*|--package-name=*)
+      PACKAGE_NAME="${i#*=}"
+      shift
+      ;;
+    -v=*|--package-version=*)
+      PACKAGE_VERSION="${i#*=}"
+      shift
+      ;;
+    -p=*|--avro-dir-path=*)
+      AVRO_FOLDER="${i#*=}"
+      shift
+      ;;
+    -o=*|--output-path=*)
+      OUTPUT_PATH="${i#*=}"
+      shift
+      ;;
+    -c=*|--company=*)
+      COMPANY="${i#*=}"
+      shift
+      ;;
+    -a=*|--authors=*)
+      AUTHORS="${i#*=}"
+      shift
+      ;;
+    *)
+      ;;
+  esac
+done
+
+echo "[Inputs]:"
+echo "  - Package name    = $PACKAGE_NAME"
+echo "  - Package version = $PACKAGE_VERSION"
+echo "  - Avro folder     = $AVRO_FOLDER"
+echo "  - Output path     = $OUTPUT_PATH"
+echo "  - Company         = $COMPANY"
+echo "  - Authors         = $AUTHORS"
+
+export PATH="$PATH:/root/.dotnet/tools"
+PACKAGE_FOLDER=./$SRC/$PACKAGE_NAME
+PROJ=$PACKAGE_FOLDER/$PACKAGE_NAME.csproj
+
+echo "Clean up..."
+rm -rf ./src
+
+echo "Creating file 'Directory.Build.props'..."
+cp /opt/build-tools/Directory.Build.props.template ./Directory.Build.props
+sed -i -e "s/{{ Company }}/$COMPANY/g" -e "s/{{ Authors }}/$AUTHORS/g"  Directory.Build.props
+
+echo "Creating $PACKAGE_NAME project..."
+dotnet new classlib --name $PACKAGE_NAME --output $SRC/$PACKAGE_NAME --framework netstandard2.0
+rm -f ./$SRC/$PACKAGE_NAME/Class1.cs
+
+echo "Adding Avro files..."
+
+count=$(find $AVRO_FOLDER -name "*.avsc" | wc -l)
+if [ "$count" -eq "0" ]; then
+  echo "Error: No Avro schema file found at '$AVRO_FOLDER'"
+  exit -1
+fi  
+
+for file in $(find $AVRO_FOLDER -name "*.avsc" -exec readlink -f {} \;)
+do
+  echo "Avro schema file found at '$file'. Trying to generate the coresponding C# class..."
+  avrogen -s $file PACKAGE_FOLDER
+done
+
+echo "Restoring packages..."
+dotnet restore $PROJ
+
+echo "Building the project..."
+dotnet build -c Release -p:AssemblyVersion=$PACKAGE_VERSION --no-restore $PROJ
+
+echo "Packing $PACKAGE_NAME version $PACKAGE_VERSION at $OUTPUT_PATH..."
+dotnet pack $PROJ -c Release --no-build --no-restore  -p:PackageVersion=$PACKAGE_VERSION -o $OUTPUT_PATH 
